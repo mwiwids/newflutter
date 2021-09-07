a60fb88c804cf779738741c0dc6fc6b318819fa7
mwiwids <m.wiwid.s@gmail.com>
2021-09-07 07:14:05 +0700

    row

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,133 +9,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(title: const Text("SFA Meet 2 - Text")),
-          body: const Center(
-            child: TypographyDemo(),
-          ),
-        ));
+      home: Scaffold(
+        appBar: AppBar(title: Text("SFA Meet 02 - Row")),
+        body: RowPage(),
+      ),
+    );
   }
 }
 
-class TypographyDemo extends StatelessWidget {
-  const TypographyDemo({Key? key}) : super(key: key);
+class RowPage extends StatefulWidget {
+  const RowPage({Key? key}) : super(key: key);
 
   @override
+  _RowPageState createState() => _RowPageState();
+}
+
+class _RowPageState extends State<RowPage> {
+  @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-    final styleItems = [
-      Text(
-        'Headline2',
-        style: textTheme.headline2?.copyWith(
-            color: Colors.grey,
-            fontWeight: FontWeight.bold,
-            fontStyle: FontStyle.italic,
-            // decoration: TextDecoration.lineThrough,
-            // decoration: TextDecoration.underline,
-            decoration: TextDecoration.combine(
-                [TextDecoration.lineThrough, TextDecoration.underline])),
-      ),
-      // headline1, headline2,.. headline6
-      // subtitle1, subtitle2
-      // bodyText1, bodyText2
-      // button, caption, overline
-      Text(
-          List.generate(10, (index) => 'Lorem ipsum dolor si amet').join(' - '),
-          // style: textTheme.subtitle1,
-          textAlign: TextAlign.justify,
-          overflow: TextOverflow.ellipsis,
-          maxLines: 2,
-          style: TextStyle(
-            fontFamily: 'TegakBersambung',
-            fontSize: 24,
-          )),
-      RichText(
-        softWrap: true,
-        textAlign: TextAlign.justify,
-        text: TextSpan(
-          children: [
-            TextSpan(
-              text: 'SISI',
-              style: TextStyle(color: Colors.red),
-            ),
-            TextSpan(
-              text: ' Flutter ',
-              style: TextStyle(color: Colors.green),
-            ),
-            TextSpan(
-              text: 'Academy ',
-              style: TextStyle(color: Colors.blue),
-            ),
-            TextSpan(
-              text: List.generate(3, (index) => 'Lorem ipsum').join(' - '),
-              style: TextStyle(color: Colors.black),
-            ),
-          ],
-        ),
-      ),
-      Container(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: RichText(
-          softWrap: true,
-          // maxLines: 1,
-          textAlign: TextAlign.justify,
-          text: TextSpan(
-            children: [
-              TextSpan(
-                text: 'SISI',
-                style: TextStyle(color: Colors.red),
-              ),
-              TextSpan(
-                text: ' Flutter ',
-                style: TextStyle(color: Colors.green),
-              ),
-              TextSpan(
-                text: 'Academy ',
-                style: TextStyle(color: Colors.blue),
-              ),
-              TextSpan(
-                text: List.generate(3, (index) => 'Lorem ipsum dolor si amet')
-                    .join(' '),
-                style: TextStyle(color: Colors.black),
-              ),
-            ],
+    return Row(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            color: Colors.red,
           ),
+          margin: EdgeInsets.all(8),
+          height: 70,
+          width: 70,
         ),
-      ),
-      RichText(
-        text: TextSpan(children: [
-          TextSpan(text: '9:30 - 2:30', style: TextStyle(color: Colors.black)),
-          WidgetSpan(
-            child: Transform.translate(
-              offset: const Offset(2, -6),
-              child: Text(
-                '+2',
-                //superscript is usually smaller in size
-                textScaleFactor: 0.7,
-                style: TextStyle(color: Colors.red),
-              ),
-            ),
-          )
-        ]),
-      ),
-      RichText(
-        text: TextSpan(children: [
-          TextSpan(text: '9:30 - 2:30', style: TextStyle(color: Colors.black)),
-          WidgetSpan(
-            child: Transform.translate(
-              offset: const Offset(2, 0),
-              child: Text(
-                '+2',
-                //superscript is usually smaller in size
-                textScaleFactor: 0.7,
-                style: TextStyle(color: Colors.red),
-              ),
-            ),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            color: Colors.green,
           ),
-        ]),
-      )
-    ];
-    return ListView(children: styleItems);
+          margin: EdgeInsets.all(8),
+          height: 70,
+          width: 70,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            color: Colors.blue,
+          ),
+          margin: EdgeInsets.all(8),
+          height: 70,
+          width: 70,
+        ),
+      ],
+    );
   }
 }
